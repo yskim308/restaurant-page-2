01167b4e6de09eb5c5a07763388f38e3c9ca73a7
yskim308 <youngseokim7@gmail.com>
2024-05-06 17:30:59 +0900

    initialize html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist');
+        path: path.resolve(__dirname, 'dist'),
     },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: './src/template.html'
+        })
+    ],
     module: {
         rules:[
             {
